5180c10bd67c88728d71c352c53de2c3177664d7
ZhuolunWu-0906 <zhuolunw@student.unimelb.edu.au>
2021-04-08 14:04:39 +1000

    Update ass1.py

--- ass1.py
+++ ass1.py
@@ -6,5 +6,16 @@ with open("melbGrid2.json", "r") as f:
     grids_data = json.load(f)
 grids = grids_data["features"]
 
+# Initialize the scoreboard
+scoreboard = {}
+for grid in grids:
+    scoreboard[grid["properties"]["id"]] = [0,0]
+print(scoreboard)
+
 # Test grid search
 print(grid_search([145.15000, -37.950000],grids))
+
+# Read tweets
+
+
+# Calculating scores
