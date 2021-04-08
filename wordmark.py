ebd08d679f0764919f0f61742c143dfff51698cf
ZhuolunWu-0906 <zhuolunw@student.unimelb.edu.au>
2021-04-08 19:03:13 +1000

    fixed ASCII bugs

--- wordmark.py
+++ wordmark.py
@@ -5,13 +5,13 @@ def text_to_score(text,wordmark_d):
     for i in range(len(head)):
         head[i]=head[i]+" "+foot[i]
     for i in head:
-       if(i.rstrip("!,?.’”") in wordmark_d.keys()):
-        #    print(i.rstrip("!,?.’”"))
-           mark += int(wordmark_d[i.rstrip("!,?.’”")])
+       if(i.rstrip("!,?.\'\"") in wordmark_d.keys()):
+        #    print(i.rstrip("!,?.\'\""))
+           mark += int(wordmark_d[i.rstrip("!,?.\'\"")])
            text=text.replace(i,'',1)
     for i in text.split():
-       if(i.rstrip("!,?.’”") in wordmark_d.keys()):
-        #    print(i.rstrip("!,?.’”"))
-           mark += int(wordmark_d[i.rstrip("!,?.’”")])
+       if(i.rstrip("!,?.\'\"") in wordmark_d.keys()):
+        #    print(i.rstrip("!,?.\'\""))
+           mark += int(wordmark_d[i.rstrip("!,?.\'\"")])
     return mark
 
