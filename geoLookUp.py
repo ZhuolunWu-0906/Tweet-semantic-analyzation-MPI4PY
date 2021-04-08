2afdf53113f6256fa7e2eec9ee2be004a4864d31
ZhuolunWu-0906 <zhuolunw@student.unimelb.edu.au>
2021-04-08 13:45:31 +1000

    Update geoLookUp.py

--- geoLookUp.py
+++ geoLookUp.py
@@ -3,6 +3,8 @@ def grid_search(geo, grids):
     letter_dict = {"A": 0, "B": 10, "C": 20, "D": 30}
     for grid in grids:
         prop = grid["properties"]
+
+        # if the coordinate is within any grid, mark the grid in with a value
         if (
             geo[0] <= prop["xmax"]
             and geo[0] >= prop["xmin"]
@@ -11,4 +13,4 @@ def grid_search(geo, grids):
         ):
             grid_dict[prop["id"]] = letter_dict[prop["id"][0]] - int(prop["id"][1])*100
 
-    return [(value, key) for key, value in grid_dict.items()]
+    return max([(value, key) for key, value in grid_dict.items()])[0]
